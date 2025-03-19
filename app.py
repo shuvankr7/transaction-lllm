85db598f67f5f2756b92c97b19f298137c164458
shuvankr7 <92281019+shuvankr7@users.noreply.github.com>
2025-03-19 15:33:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,28 +28,28 @@ def process_transaction_message(message, llm):
 "Check if this message is a valid transaction message or not. "
 "If valid, extract the following data: Amount, Transaction Type, Bank Name, Card Type, paied to whom,marchent, Transaction Mode, Transaction Date, Reference Number, and tag."
 """example 1: messsage - Rs.105.00 spent on your SBI Credit Card ending with 5775 at Auto Fuel Station on 18-03-25 via UPI (Ref No. 507775912830). Trxn. not done by you? Report at https://sbicard.com/Dispute , output :{
-"Amount":105
-"Transaction Type":"Debit"
-"Bank Name":"SBI"
-"Card Type":"Credit Card"
-"marchent":"Auto Fuel Station"
-"paied to whom":"Auto Fuel Station"
-"Transaction Mode":"Credit Card"
-"Transaction Date":"19-03-25"
-"Reference Number":"507775912830"
+"Amount":105,
+"Transaction Type":"Debit",
+"Bank Name":"SBI",
+"Card Type":"Credit Card",
+"marchent":"Auto Fuel Station",
+"paied to whom":"Auto Fuel Station",
+"Transaction Mode":"Credit Card",
+"Transaction Date":"19-03-25",
+"Reference Number":"507775912830",
 "tag":["Transport"]
 } """
 """example 2 : ICICI Bank Acct XX337 debited for Rs 500.00 on 17-Jan-25; BPCL Ufill 2 credited. UPI:501714256060. Call 18002662 for dispute. SMS BLOCK 337 to 9215676766.
 output :{
-"Amount":500
-"Transaction Type":"Debit"
-"Bank Name":"ICICI"
-"Card Type":NULL
-"Merchant":"BPCL"
-"paied to whom":"BPCL"
-"Transaction Mode":"UPI"
-"Transaction Date":"19-03-25"
-"Reference Number":NULL
+"Amount":500,
+"Transaction Type":"Debit",
+"Bank Name":"ICICI",
+"Card Type":NULL,
+"Merchant":"BPCL",
+"paied to whom":"BPCL",
+"Transaction Mode":"UPI",
+"Transaction Date":"19-03-25",
+"Reference Number":NULL,
 "tag":["Transport"]
 }"""
 "Tag meaning which category of spending, if amazon then shopping etc, if zomato then eating"
